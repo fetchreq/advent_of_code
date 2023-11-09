3f5a55b47715d9b1ec6d87e83f4559812ad984af
Ryan <rjprice04@gmail.com>
2023-11-08 21:18:38 -0600

    Day 7

---
+++ cast/cast.go
@@ -0,0 +1,37 @@
+package cast
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ToInt(arg interface{}) int {
+	var val int
+	switch arg.(type) {
+	case string:
+		var err error
+		val, err = strconv.Atoi(arg.(string))
+		if err != nil {
+			panic("error converting string to int " + err.Error())
+		}
+	default:
+		panic(fmt.Sprintf("unhandled type for int casting %T", arg))
+	}
+	return val
+}
+
+func ToString(arg interface{}) string {
+	var str string
+	switch arg.(type) {
+	case int:
+		str = strconv.Itoa(arg.(int))
+	case byte:
+		b := arg.(byte)
+		str = string(rune(b))
+	case rune:
+		str = string(arg.(rune))
+	default:
+		panic(fmt.Sprintf("unhandled type for string casting %T", arg))
+	}
+	return str
+}
