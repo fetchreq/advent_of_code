b6c4ba7f373ad7e7403721fae2b0a3cab6563f53
Ryan <rjprice04@gmail.com>
2023-12-02 19:11:06 -0600

    Move Download and Create Logic to Seperate Files

--- util/utils.go
+++ util/utils.go
@@ -15,7 +15,7 @@ import (
 // The year is the sub year folder
 // day is the input for the day
 // test is if we should use test file or real input
-func ReadFile(year string, day string, test bool) string {
+func ReadFile(year, day string, test bool) string {
 	path := "./input/" + year + "/day" + day
 	if test {
 		path += ".test"
@@ -28,43 +28,57 @@ func ReadFile(year string, day string, test bool) string {
 	// if we get an error make a request to get the data from AoC site
 	if err != nil {
 		fmt.Println("Input file Not Found")
-		fmt.Println("Downloading..." )
-		// probably need to download the file
-		viper.SetConfigFile(".env")
-		err := viper.ReadInConfig()
-		CheckErr(err)
-
-		session := viper.Get("SESSION").(string)
-		url := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, day)
-
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", url, nil)
-		CheckErr(err)
-		req.AddCookie(&http.Cookie{Name: "session", Value: session})
-		
-
-		res, err := client.Do(req) 
-		CheckErr(err)
-
-		resBody, err := io.ReadAll(res.Body)
-		CheckErr(err)
-		
-		fmt.Println("Creating File..." )
-		file, err := os.Create(path)
-		CheckErr(err)
-		defer file.Close()
-
-		num, err := file.Write(resBody)
-		CheckErr(err)
-		fmt.Printf("Wrote %d bytes\n", num)
-		data = resBody
-
+		input := getProblemInput(year, day)
+		bytesWritten := createFileWithData(year, day, input)
+		fmt.Printf("Wrote %d bytes\n", bytesWritten)
 
 	} else {
 		fmt.Println("Using Local File")
 	}
 	strContent := string(data)
 	return strings.TrimRight(strContent, "\n")
+}
+
+// Downloads the input file from advent of code website
+func getProblemInput(year, day string) []byte {
+	fmt.Println("Downloading..." )
+
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	CheckErr(err)
+
+	session := viper.Get("SESSION").(string)
+	url := fmt.Sprintf("https://adventofcode.com/%s/day/%s/input", year, day)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	CheckErr(err)
+	req.AddCookie(&http.Cookie{Name: "session", Value: session})
+
+
+	res, err := client.Do(req) 
+	CheckErr(err)
+
+	resBody, err := io.ReadAll(res.Body)
+	CheckErr(err)
+
+	return resBody
+}
+
+// Creates a file ./input/YEAR/dayXX.txt with the input bytes 
+func createFileWithData(year, day string, data []byte) int {
+	path := "./input/" + year + "/day" + day
+
+	fmt.Println("Creating File..." )
+	file, err := os.Create(path)
+	CheckErr(err)
+	defer file.Close()
+
+	num, err := file.Write(data)
+	CheckErr(err)
+	fmt.Printf("Wrote %d bytes\n", num)
+	
+	return num
 
 }
 
